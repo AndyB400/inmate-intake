3d8592750bdf3c2aa7536a30b7e14079ded58fbd
Andy <andy.brad@gmail.com>
2020-04-10 23:14:17 +0100

    Added missing HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { CoreModule } from 'core/core.module';
 import { HomeModule } from 'home/home.module';
 import { MaterialModule } from 'shared/material.module';
@@ -20,6 +20,7 @@ import { SharedModule } from 'shared/shared.module';
     BrowserModule,
     CoreModule,
     HomeModule,
+    HttpClientModule,
     MaterialModule,
     SharedModule
   ],
