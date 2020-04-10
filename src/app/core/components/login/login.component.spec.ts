da9ff8dc1cc4b2659e85b9c3095cb073f9fc167c
Andy <andy.brad@gmail.com>
2020-04-10 20:41:41 +0100

    Added login component

---
+++ src/app/core/components/login/login.component.spec.ts
@@ -0,0 +1,37 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'shared/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { LoginComponent } from './login.component';
+
+describe('LoginComponent', () => {
+    let component: LoginComponent;
+    let fixture: ComponentFixture<LoginComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [LoginComponent],
+            imports: [
+                ReactiveFormsModule,
+                MaterialModule,
+                RouterTestingModule,
+                BrowserAnimationsModule,
+                HttpClientTestingModule
+            ],
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
---
+++ src/app/core/components/login/login.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { AuthService } from 'core/services';
+import { LoginCredential } from 'shared/models';
+
+@Component({
+  selector: 'ii-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loginCredential: LoginCredential = {
+    username: '',
+    password: '',
+  };
+
+  invalidLogin = false;
+  returnUrl: string;
+
+  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService) {}
+
+  ngOnInit() {
+    // reset login status
+    this.authService.logout();
+
+    // Get the query params
+    this.route.queryParams.subscribe(params => {
+      this.returnUrl = params['returnUrl'] || '/home';
+
+      if (this.authService.isAuthenticated()) {
+        this.router.navigate([this.returnUrl]);
+      }
+    });
+
+    this.resetErrorHandling();
+    this.createForm(this.loginCredential);
+  }
+
+  private createForm(loginCredential: LoginCredential) {
+    this.loginForm = new FormGroup({
+      username: new FormControl(loginCredential.username, [Validators.required, Validators.maxLength(100)]),
+      password: new FormControl(loginCredential.password, [Validators.required, Validators.maxLength(100)]),
+    });
+  }
+
+  submitForm(formValues) {
+    this.resetErrorHandling();
+
+    if (!this.loginForm.valid) {
+      return;
+    }
+
+    this.loginCredential.username = formValues.username;
+    this.loginCredential.password = formValues.password;
+
+    this.login();
+  }
+
+  private resetErrorHandling() {
+    this.invalidLogin = false;
+  }
+
+  private login() {
+    this.authService.login(this.loginCredential).subscribe(
+      _ => {
+        this.router.navigateByUrl(this.returnUrl);
+      },
+      error => {
+        this.invalidLogin = true;
+        throw error;
+      }
+    );
+  }
+
+  getUsernameErrorMessage() {
+    const username = this.loginForm.controls.username;
+
+    return username.hasError('required')
+      ? 'Required'
+      : username.hasError('maxlength')
+      ? 'Max Length 100 characters'
+      : '';
+  }
+
+  getPasswordErrorMessage() {
+    const password = this.loginForm.controls.password;
+
+    return password.hasError('required')
+      ? 'Required'
+      : password.hasError('maxlength')
+      ?  'Max Length 100 characters'
+      : '';
+  }
+}
