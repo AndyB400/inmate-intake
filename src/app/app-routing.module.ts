f1d4d2087a8540c429abc0e8a8bf50e482db5720
Andy <andy.brad@gmail.com>
2020-04-10 20:41:17 +0100

    First pass setting up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent, UnauthorisedComponent } from 'core/components';
+import { AuthGuard } from 'core/guards';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'unauthorised', component: UnauthorisedComponent, canActivate: [AuthGuard] },
+  {
+      path: 'home',
+      loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+      canLoad: [AuthGuard]
+  },
+  {
+      path: 'inmate',
+      loadChildren: () => import('./inmate/inmate.module').then(m => m.InmateModule),
+      canLoad: [AuthGuard]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
