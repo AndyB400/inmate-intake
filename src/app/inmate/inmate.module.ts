be2ed8bbbda06bb3cfcc3664d49c3c0ab3999db2
Andy <andy.brad@gmail.com>
2020-04-11 19:25:58 +0100

    Added inmate resolver for edit page

--- src/app/inmate/inmate.module.ts
+++ src/app/inmate/inmate.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
+import { MaterialModule } from 'shared/material.module';
+import { SharedModule } from 'shared/shared.module';
+import { InmateResolver } from './inmate.resolver';
+import { InmateRoutingModule } from './inmate-routing.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule, FormsModule, ReactiveFormsModule, MaterialModule, SharedModule, InmateRoutingModule
+  ],
+  providers: [InmateResolver]
 })
 export class InmateModule { }
