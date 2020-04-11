ca1820c3eba857702bbea52865ad581324fa2a92
Andy <andy.brad@gmail.com>
2020-04-11 19:19:50 +0100

    Regenerated json data with shorted dates to work with JS

--- src/app/shared/models/inmate/inmate.ts
+++ src/app/shared/models/inmate/inmate.ts
@@ -15,21 +15,21 @@ export interface Inmate {
 
 {
   "inmates": [
-    '{{repeat(5, 500)}}',
+    '{{repeat(500)}}',
     {
       id: '{{index()}}',
       firstNames: '{{firstName()}}',
       lastName: '{{surname()}}',
-      dob: '{{date(new Date(1950, 0, 1), new Date(2000, 0, 1), "YYYY-MM-ddThh:mm:ss Z")}}',
+      dob: '{{date(new Date(1950, 0, 1), new Date(2000, 0, 1), "YYYY-MM-dd")}}',
       cellNumber: '{{integer(1, 400)}}',
-      intakeDate: '{{date(new Date(2014, 0, 1), new Date(), "YYYY-MM-ddThh:mm:ss Z")}}',
+      intakeDate: '{{date(new Date(2014, 0, 1), new Date(), "YYYY-MM-ddThh:mm:ss")}}',
       isActive: '{{bool()}}',
       locationHistory: [
         '{{repeat(0,5)}}',
         {
           id: '{{index()}}',
           location: '{{random("In cell", "Cafeteria", "Gym", "Kitchen", "Visitors Room", "Phone", "Infirmary")}}',
-          timestamp: '{{date(new Date(2020, 3, 9), new Date(), "YYYY-MM-ddThh:mm:ss Z")}}'
+          timestamp: '{{date(new Date(2020, 3, 9), new Date(), "YYYY-MM-ddThh:mm:ss")}}'
         }
       ]
     }
