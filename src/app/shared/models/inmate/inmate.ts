9ce7c7f9a5bd5bc8a7469c38c6f239df8d3f98fa
Andy <andy.brad@gmail.com>
2020-04-11 11:55:05 +0100

    Added formala for JSON Generator for use later

--- src/app/shared/models/inmate/inmate.ts
+++ src/app/shared/models/inmate/inmate.ts
@@ -9,3 +9,31 @@ export interface Inmate {
   isActive: boolean;
   locationHistory: LocationHistory[];
 }
+
+
+/* JSON Generator Formula
+
+{
+  "inmates": [
+    '{{repeat(5, 500)}}',
+    {
+      id: '{{index()}}',
+      firstNames: '{{firstName()}}',
+      lastName: '{{surname()}}',
+      cellNumber: '{{integer(1, 400)}}',
+      dob: '{{date(new Date(1950, 0, 1), new Date(2000, 0, 1), "YYYY-MM-ddThh:mm:ss Z")}}',
+      intakeDate: '{{date(new Date(2014, 0, 1), new Date(), "YYYY-MM-ddThh:mm:ss Z")}}',
+      isActive: '{{bool()}}',
+      locationHistory: [
+        '{{repeat(0,5)}}',
+        {
+          id: '{{index()}}',
+          location: '{{random("In cell", "Cafeteria", "Gym", "Kitchen", "Visitors Room", "Phone", "Infirmary")}}',
+          timestamp: '{{date(new Date(2020, 3, 9), new Date(), "YYYY-MM-ddThh:mm:ss Z")}}'
+        }
+      ]
+    }
+  ]
+}
+
+*/
