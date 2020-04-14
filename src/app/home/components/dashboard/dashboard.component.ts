5fde5e1ce4c3edf5bbcae753578028501b9c9de1
Andy <andy.brad@gmail.com>
2020-04-14 10:03:10 +0100

    Amended dashboad to inlcude expanding row functionality to display location history

--- src/app/home/components/dashboard/dashboard.component.ts
+++ src/app/home/components/dashboard/dashboard.component.ts
@@ -5,13 +5,21 @@ import { MatSort } from '@angular/material/sort';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { finalize } from 'rxjs/operators';
 
-import { Inmate } from 'shared/models';
+import { Inmate, LocationHistory } from 'shared/models';
 import { InmateService } from 'shared/services';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'ii-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class DashboardComponent implements OnInit {
   loading = false;
@@ -19,11 +27,12 @@ export class DashboardComponent implements OnInit {
   private activeToggle = true;
   private filterValue = '';
 
-  displayedColumns: string[] = ['firstNames', 'lastName', 'age', 'intakeDate', 'cellNumber'];
+  displayedColumns: string[] = ['firstNames', 'lastName', 'age', 'intakeDate', 'cellNumber', 'locations'];
   dataSource: MatTableDataSource<Inmate>;
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
+  expandedInmate: Inmate | null;
 
   constructor(private inmateService: InmateService, private snackBar: MatSnackBar) { }
 
@@ -79,11 +88,15 @@ export class DashboardComponent implements OnInit {
 
     const filtered = this.inmates.filter((item: Inmate) => {
       return (item.firstNames.toLocaleLowerCase().indexOf(filterValue) !== -1 ||
-        item.lastName.toLocaleLowerCase().indexOf(filterValue) !== -1  ||
+        item.lastName.toLocaleLowerCase().indexOf(filterValue) !== -1 ||
         item.cellNumber.toString().indexOf(filterValue) !== -1)
         && (showActive ? item.isActive : true);
     });
 
     this.bindTable(filtered);
+  }
+
+  getActiveLocationHistory(inmate: Inmate): LocationHistory[] {
+    return inmate.locationHistory.filter(lh => lh.isActive === true);
   }
 }
